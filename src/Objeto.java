2dabb0e90343c5268002f31e6dbe7ad6040b45a0
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-06-09 21:35:44 -0500

    agregue constructor

--- src/Objeto.java
+++ src/Objeto.java
@@ -3,5 +3,14 @@ public class Objeto {
     String nombre;
     double valor;
     double peso;
+    //constructor
+        public Objeto(String nombre, double valor, double peso) {
+        this.nombre = nombre;
+        this.valor = valor;
+        this.peso = peso;
+    }
 
+    public double getRelacion() {
+        return valor / peso;
+    }
 }

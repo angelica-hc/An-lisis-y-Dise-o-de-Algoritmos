9be5b4200cc897ab9b1c82326eb2c9d68b2690a5
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-06-09 21:10:15 -0500

    agregue atributos

--- src/Objeto.java
+++ src/Objeto.java
@@ -1,16 +1,7 @@
 public class Objeto {
-
+//atributos del objeto
     String nombre;
     double valor;
     double peso;
 
-    public Objeto(String nombre, double valor, double peso) {
-        this.nombre = nombre;
-        this.valor = valor;
-        this.peso = peso;
-    }
-
-    public double getRelacion() {
-        return valor / peso;
-    }
 }

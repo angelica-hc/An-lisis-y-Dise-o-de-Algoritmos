41db12f02983c646c69f2e3fe6c8894ee0dd4a0b
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-06-29 22:46:56 -0500

    Update Escaleras.cpp

--- Escaleras.cpp
+++ Escaleras.cpp
@@ -11,5 +11,13 @@ if(n>=1)
 for(int i=2;i<=n; i++){
   dp [i]=dp[i-1]+dp[i-2];
 }
+  cout<<"\nformas posibles:" << dp[n]<<endl;
+  cout <<"tabla dp: [";
+  for (inti=0; i<=n; i++){
+    cout << dp [i];
+    if(i !=n)
+      cout<<", ";
+  }
+  cout <<"]";
 }
 

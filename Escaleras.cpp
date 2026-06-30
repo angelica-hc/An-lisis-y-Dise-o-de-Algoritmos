25d63da98a41f6c443bdc3390c1944b124901de1
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-06-29 22:43:59 -0500

    Update Escaleras.cpp

--- Escaleras.cpp
+++ Escaleras.cpp
@@ -4,4 +4,12 @@ int main () {
   int n;
 cout <<"indicar numero de escalones:";
 cin>> n;
+long long dp[100];
+dp[0]=1;
+if(n>=1)
+  dp[1]=1;
+for(int i=2;i<=n; i++){
+  dp [i]=dp[i-1]+dp[i-2];
 }
+}
+
